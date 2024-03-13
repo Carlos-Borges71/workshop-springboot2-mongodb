8aae09dc0f5066714a62321ceaf0b534c21661eb
Carlos Borges <kaca_master@hotail.com>
2024-03-13 16:39:52 -0300

    Obtendo todos os post e por id

--- src/main/java/com/carlosborges/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carlosborges/workshopmongo/config/Instantiation.java
@@ -39,11 +39,17 @@ public class Instantiation implements CommandLineRunner {
 		postRepository.deleteAll();
 		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!" , new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
-		
-		postRepository.saveAll(Arrays.asList(post1, post2));
+		Post post3 = new Post(null,sdf.parse("30/06/2022"), "Partiu praia","Vou viajar para Rio. Abraços!" , new AuthorDTO(alex));
+		Post post4 = new Post(null,sdf.parse("23/05/2018"), "Bom dia","Feliz hoje!", new AuthorDTO(bob));
+		postRepository.saveAll(Arrays.asList(post1, post2,post3, post4));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		alex.getPosts().addAll(Arrays.asList(post3));
+		bob.getPosts().addAll(Arrays.asList(post4));
 		userRepository.save(maria);
+		userRepository.save(alex);
+		userRepository.save(bob);
+		
 		
 	}
 	
---
+++ src/main/java/com/carlosborges/workshopmongo/resouses/PostResouce.java
@@ -0,0 +1,38 @@
+package com.carlosborges.workshopmongo.resouses;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlosborges.workshopmongo.domain.Post;
+import com.carlosborges.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResouce {
+	
+	@Autowired
+	private PostService service;
+	
+	//@GetMapping
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll(){		
+		List<Post> list = service.findAll();		
+		return ResponseEntity.ok().body(list); 
+	}
+	
+		//@GetMapping
+		@RequestMapping(value="/{id}",method=RequestMethod.GET)
+		public ResponseEntity<Post> findById(@PathVariable String id){	
+			Post obj = service.findById(id);			
+			return ResponseEntity.ok().body(obj); 
+		}
+		
+		
+
+}
